84073e892728facfe8bd48fc282774601cec89a6
Anatoly Prohacky <tolikproh@mail.ru>
2025-02-04 20:39:58 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,101 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var (
+	ErrInvalidString      = errors.New("invalid string")
+	ErrStringIsNotUtf8    = errors.New("string is not UTF-8 encoded")
+	ErrBackSlashEndString = errors.New("string ends with backslash")
+	ErrStartsWithDigit    = errors.New("string starts with digit")
+	ErrConsecutiveDigits  = errors.New("consecutive digits not allowed")
+)
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+// Проврека, что rune число.
+func isDigit(r rune) bool {
+	return unicode.IsDigit(r)
+}
+
+// Проврека, что rune обратный слэш.
+func isBackSlash(r rune) bool {
+	return r == '\\'
+}
+
+// Функция Unpack.
+func Unpack(in string) (string, error) {
+	// Проверка строки на соответвие UTF8.
+	if !utf8.ValidString(in) {
+		return "", ErrStringIsNotUtf8
+	}
+
+	// Перевод строки в массив rune для последующей работы с ней.
+	runes := []rune(in)
+	maxLen := len(runes)
+
+	// Проверка если пустая строка.
+	if maxLen == 0 {
+		return "", nil
+	}
+	// Проверка если первый символ число.
+	if isDigit(runes[0]) {
+		return "", ErrStartsWithDigit
+	}
+	// Проверка если последний символ обратный слэш.
+	if isBackSlash(runes[maxLen-1]) {
+		return "", ErrBackSlashEndString
+	}
+
+	// Инициализация в памяти переменной.
+	var result strings.Builder
+	result.Grow(maxLen * 2)
+
+	for i := 0; i < maxLen; i++ {
+		// Читаем текущий символ.
+		curr := runes[i]
+
+		// Выход с ошибкой если сначала идет число.
+		if isDigit(curr) {
+			return "", ErrConsecutiveDigits
+		}
+
+		// Проверка на обратный слэш,
+		// если это так то читаем следующий символ и проверяем его,
+		// что он цифра или обратный слэш, иначе выход с ошибкой.
+		if isBackSlash(curr) {
+			i++
+			curr = runes[i]
+			if !isDigit(curr) && !isBackSlash(curr) {
+				return "", ErrInvalidString
+			}
+		}
+
+		// Проверка, что следующий символ число,
+		// Если это так, то делаем повтор символов на данное число,
+		// Записываем результат и идем в начало цикла.
+		if maxLen > i+1 && isDigit(runes[i+1]) {
+			i++
+			count, err := strconv.Atoi(string(runes[i]))
+			if err != nil {
+				return "", err
+			}
+
+			if _, err := result.WriteString(strings.Repeat(string(curr), count)); err != nil {
+				return "", err
+			}
+			continue
+		}
+
+		//  Если следующий символ не число,
+		// то записываем текущий символ и идем в начало цикла.
+		if _, err := result.WriteRune(curr); err != nil {
+			return "", err
+		}
+	}
+
+	// Выход с возвратом результата.
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -18,15 +18,36 @@ func TestUnpack(t *testing.T) {
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		// uncomment if task with asterisk completed.
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\5`, expected: `qwe\5`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		// мои тесты.
+		{input: `\53w4e`, expected: `555wwwwe`},
+		{input: `a9b9c9d9`, expected: `aaaaaaaaabbbbbbbbbcccccccccddddddddd`},
+		{input: `\15t\4\5`, expected: `11111t45`},
+		{input: `t0a\40\55g0`, expected: `a55555`},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "d\t3ab0c", expected: "d\t\t\tac"},
+		{input: "日3本語2", expected: "日日日本語語"},
+		{input: `😈2😅1😎😇0ен\5`, expected: `😈😈😅😎ен5`},
+		{input: "🙉4🙊0🙋\r3", expected: "🙉🙉🙉🙉🙋\r\r\r"},
+		{input: "ジⅥ✌1💬\v2", expected: "ジⅥ✌💬\v\v"},
+		{input: " 4💬5\f3", expected: "    💬💬💬💬💬\f\f\f"},
+		{input: "f2 3ds", expected: "ff   ds"},
+		{input: " ", expected: " "},
+		{input: "\"3f0g4", expected: "\"\"\"gggg"},
+		{input: `\\4`, expected: `\\\\`},
+		{input: `\4`, expected: `4`},
+		{input: `d3-5`, expected: `ddd-----`},
+		{input: "п2р3с4", expected: "ппрррсссс"},
+		{input: "!@4#2", expected: "!@@@@##"},
+		{input: `\4\5abc3`, expected: "45abccc"},
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -35,13 +56,29 @@ func TestUnpack(t *testing.T) {
 	}
 }
 
-func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
-	for _, tc := range invalidStrings {
-		tc := tc
-		t.Run(tc, func(t *testing.T) {
-			_, err := Unpack(tc)
-			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+func TestUnpackError(t *testing.T) {
+	tests := []struct {
+		input       string
+		expectedErr error
+	}{
+		{input: "3abc", expectedErr: ErrStartsWithDigit},
+		{input: "45", expectedErr: ErrStartsWithDigit},
+		{input: "aaa10b", expectedErr: ErrConsecutiveDigits},
+		{input: `qw\ne`, expectedErr: ErrInvalidString},
+		{input: `dhg43d\`, expectedErr: ErrBackSlashEndString},
+		{input: `"日3\本2"`, expectedErr: ErrInvalidString},
+		{input: `2😅1😎😇0ен`, expectedErr: ErrStartsWithDigit},
+		{input: `😅0ен\`, expectedErr: ErrBackSlashEndString},
+		{input: `\`, expectedErr: ErrBackSlashEndString},
+		{input: `3\`, expectedErr: ErrStartsWithDigit},
+		{input: `d3\-5`, expectedErr: ErrInvalidString},
+		{input: string([]byte{0xFF, 0xFF}), expectedErr: ErrStringIsNotUtf8},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			_, err := Unpack(tc.input)
+			require.Truef(t, errors.Is(err, tc.expectedErr), "actual error %q", err)
 		})
 	}
 }
