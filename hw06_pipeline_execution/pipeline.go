6f37d70f081b321bcadf6aff5cce4dd8e582faf3
Anatoly Prohacky <tolikproh@mail.ru>
2025-03-15 21:23:34 +0300

    HW06 is complited

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,39 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if in == nil {
+		out := make(Bi)
+		close(out)
+		return out
+	}
+
+	for _, stage := range stages {
+		in = execStage(in, done, stage)
+	}
+
+	return in
+}
+
+func execStage(in In, done In, stage Stage) Bi {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+
+		in = stage(in)
+
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- val
+			}
+		}
+	}()
+
+	return out
 }
