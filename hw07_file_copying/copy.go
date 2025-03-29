aa124707ff9015df0f8eb1355451ca58a40813bf
Anatoly Prohacky <tolikproh@mail.ru>
2025-03-29 14:27:28 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	pb "github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,58 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if fromPath == "" || toPath == "" {
+		return ErrUnsupportedFile
+	}
+
+	sourceFileInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	sourceFileSize := sourceFileInfo.Size()
+	if sourceFileSize == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > sourceFileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 || limit > sourceFileSize-offset {
+		limit = sourceFileSize - offset
+	}
+
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	if offset > 0 {
+		if _, err := sourceFile.Seek(offset, io.SeekStart); err != nil {
+			return err
+		}
+	}
+
+	destFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	progressBar := pb.Full.Start64(limit)
+	sourceFileProgress := progressBar.NewProxyReader(sourceFile)
+
+	_, err = io.CopyN(destFile, sourceFileProgress, limit)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		return err
+	}
+
+	progressBar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,56 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("error file is not exist", func(t *testing.T) {
+		pathFrom := "./tmp1"
+		pathTo := "./tmp2"
+		t.Cleanup(func() {
+			_ = os.Remove(pathTo)
+		})
+		err := Copy(pathFrom, pathTo, 0, 0)
+		require.Error(t, err)
+		require.True(t, errors.Is(err, os.ErrNotExist), "Ожидается ошибка os.ErrNotExist, получена: %v", err)
+	})
+
+	t.Run("file size null", func(t *testing.T) {
+		pathFrom := "/dev/urandom"
+		pathTo := "./tmp2"
+
+		t.Cleanup(func() {
+			_ = os.Remove(pathTo)
+		})
+
+		err := Copy(pathFrom, pathTo, 0, 0)
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrUnsupportedFile), "Ожидается ошибка ErrUnsupportedFile, получина: %v", err)
+	})
+
+	t.Run("offset is not valid", func(t *testing.T) {
+		pathFrom := "testdata/input.txt"
+		pathTo := "tmp.txt"
+
+		fileFromInfo, err := os.Stat(pathFrom)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fileFromSize := fileFromInfo.Size()
+		offset := fileFromSize + 1
+
+		t.Cleanup(func() {
+			_ = os.Remove(pathTo)
+		})
+
+		err = Copy(pathFrom, pathTo, offset, 0)
+		require.Error(t, err)
+		require.True(t, errors.Is(err, ErrOffsetExceedsFileSize), "Ожидается ошибка ErrOffsetExceedsFileSize, получена: %v", err)
+	})
 }
